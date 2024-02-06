5cbc69f7d7e32b1deac014a3ee833f0bfbbd810e
AndrewDiv <65342165+AndrewDiv@users.noreply.github.com>
2024-02-06 23:09:59 +0300

    added decision_functions.py
    decision functions for an arbitrary number of classes

--- fcalc/decision_functions.py
+++ fcalc/decision_functions.py
@@ -1,13 +1,26 @@
 import numpy as np
 
-def alpha_weak(supp_cont, class_lengths, alpha=0.):
+def alpha_weak(supp_cont,classes, class_lengths, alpha=0.):
     ccl = class_lengths.sum() - class_lengths
-    criter = np.zeros(shape=(class_lengths,supp_cont[0].shape[1]))
-    for j in range(len(class_lengths)):
+    criter = np.zeros(shape=(len(class_lengths),supp_cont[0].shape[1]))
+    for j in range(len(classes)):
         criter[j] = (supp_cont[j][1] <= ccl[j] * alpha).sum(axis=-1)
-    
-    criter /= class_lengths
-    # if (criter == max(criter)).sum() > 1:
-    #     return -1, criter
+    criter = criter.T / class_lengths
+    return classes[np.argmax(criter, axis=-1)]
 
-    return np.argmax(criter, axis=-1) #, criter
+def alpha_weak_support(supp_cont, classes, class_lengths, alpha=0.):
+    ccl = class_lengths.sum() - class_lengths
+    criter = np.zeros(shape=(len(class_lengths),supp_cont[0].shape[1]))
+    for j in range(len(classes)):
+        criter[j] = (supp_cont[j][0]*(supp_cont[j][1] <= ccl[j] * alpha)).sum(axis=-1)
+    criter = criter.T / class_lengths**2
+    return classes[np.argmax(criter, axis=-1)]
+
+def ratio_support(supp_cont, classes, class_lengths, alpha=1.):
+    ccl = class_lengths.sum() - class_lengths
+    criter = np.zeros(shape=(len(class_lengths),supp_cont[0].shape[1]))
+    for j in range(len(classes)):
+        sup = (supp_cont[j][0]*(supp_cont[j][1]/ccl[j] * alpha < supp_cont[j][0]/class_lengths[j])).sum(axis=-1)
+        cont = (supp_cont[j][1]*(supp_cont[j][1]/ccl[j] * alpha < supp_cont[j][0]/class_lengths[j])).sum(axis=-1)+1e-6
+        criter[j] = sup / cont * (ccl[j]/class_lengths[j])
+    return classes[np.argmax(criter.T, axis=-1)]
